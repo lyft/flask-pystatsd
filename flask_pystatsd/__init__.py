f48df87543f77295d15dfe5cd69e6cdf4d0f590b
elijahchancey <elijah.chancey@lyft.com>
2013-11-15 17:35:23 -0800

    .

--- flask_pystatsd/__init__.py
+++ flask_pystatsd/__init__.py
@@ -16,14 +16,7 @@ class SendMetric(object):
             self.init_app(app)
 
     def init_app(self, app):
-        app.config.setdefault('host', '127.0.0.1')
-        # This is the prefix component of the metric name. It depends on the config/environment variable CLUSTER_NAME which is set through 'heroku config:set'
-        prefix = os.environ['CLUSTER_NAME']
-        # This is the suffix component of the metric name. It gets the UUID which is also the hostname.
-        suffix =  "heroku_" + os.popen('hostname').read().rstrip()
-        # Where we send our stats to.
-        host = os.environ['STATSD_HOSTNAME']
-        port = 8127
+        
         # Use the newstyle teardown_appcontext if it's available, otherwise fall back to the request context.
         #if hasattr(app, 'teardown_appcontext'):
         #    app.teardown_appcontext(self.teardown)
@@ -31,6 +24,17 @@ class SendMetric(object):
         #    app.teardown_request(self.teardown)
 
     def connect(self):
+        # This is the prefix component of the metric name. It depends on the config/environment variable CLUSTER_NAME which is set through 'heroku config:set'
+        prefix = 'no_cluster_name_defined'
+        prefix = os.environ['CLUSTER_NAME']
+        # This is the suffix component of the metric name. It gets the UUID which is also the hostname.
+        suffix = 'no_hostname_defined'
+        suffix =  "heroku_" + os.popen('hostname').read().rstrip()
+        # Where we send our stats to.
+        host = '127.0.0.1'
+        host = os.environ['STATSD_HOSTNAME']
+        port = 8127
+        
         return statsd.StatsClient(host=host, port=port, prefix=prefix, suffix=suffix)
 
     #def teardown(self, exception):
