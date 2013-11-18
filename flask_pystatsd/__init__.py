bd5619af083420947a56b0773dcfb751b8d80957
elijahchancey <elijah.chancey@lyft.com>
2013-11-18 15:46:27 -0800

    .

--- flask_pystatsd/__init__.py
+++ flask_pystatsd/__init__.py
@@ -10,6 +10,6 @@ except:
 
 def setup_app(app):
     app.statsd = statsd.StatsClient(host=os.environ.get('STATSD_HOSTNAME', '127.0.0.1'),
-    port=(os.environ.get('STATSD_PORT', '8125'),
+    port=os.environ.get('STATSD_PORT', '8125'),
     prefix=os.environ.get('CLUSTER_NAME', 'None'),
     suffix=suffix)
