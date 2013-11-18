8073b1c05d26811943d449c6c7f530571e161a65
elijahchancey <elijah.chancey@lyft.com>
2013-11-18 11:08:58 -0800

    .

--- flask_pystatsd/__init__.py
+++ flask_pystatsd/__init__.py
@@ -3,10 +3,10 @@ import os
 from flask import current_app
 
 try:
-  # This is the suffix component of the metric name. It gets the UUID which is also the hostname.
-  suffix =  "heroku_" + os.popen('hostname').read().rstrip()
+    # This is the suffix component of the metric name. It gets the UUID which is also the hostname.
+    suffix = "heroku_" + os.popen('hostname').read().rstrip()
 except:
-  suffix = None
+    suffix = None
 
 current_app.statsd = statsd.StatsClient(host=os.environ.get('STATSD_HOSTNAME', None),
     port=os.environ.get('STATSD_PORT', None),
