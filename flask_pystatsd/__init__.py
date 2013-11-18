68a7f9fe6de3be528e7631844bddff513c60221d
elijahchancey <elijah.chancey@lyft.com>
2013-11-18 08:54:14 -0800

    .

--- flask_pystatsd/__init__.py
+++ flask_pystatsd/__init__.py
@@ -24,6 +24,7 @@ class SendMetric(object):
         #    app.teardown_request(self.teardown)
 
     def connect(self):
+        # use try & catch
         # This is the prefix component of the metric name. It depends on the config/environment variable CLUSTER_NAME which is set through 'heroku config:set'
         prefix = 'no_cluster_name_defined'
         prefix = os.environ['CLUSTER_NAME']
@@ -31,12 +32,14 @@ class SendMetric(object):
         suffix = 'no_hostname_defined'
         suffix =  "heroku_" + os.popen('hostname').read().rstrip()
         # Where we send our stats to.
-        host = '127.0.0.1'
-        host = os.environ['STATSD_HOSTNAME']
+        #host = '127.0.0.1'
+        #host = os.environ['STATSD_HOSTNAME']
+        host = 'statsd-development-iad.lyft.net'
         port = 8127
         
         return statsd.StatsClient(host=host, port=port, prefix=prefix, suffix=suffix)
 
+        
     #def teardown(self, exception):
     #   ctx = stack.top
         #if hasattr(ctx, 'sqlite3_db'):
@@ -55,7 +58,6 @@ class SendMetric(object):
     
     def timer(self):
         connect.timing('KEY', VALUE)
-
 
 # This sets up the UDP connection to the statsd server.
         # catch exception try/catch in pystatsd, send udp packet to 127
