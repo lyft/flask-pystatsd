e25757437106035b15cf244b3f400b46e12154e3
Mathias Gug <mathias@lyft.com>
2013-11-18 10:34:55 -0800

    Update __init__.py

--- flask_pystatsd/__init__.py
+++ flask_pystatsd/__init__.py
@@ -6,26 +6,24 @@ class SendMetric(object):
 
   def __init__(self, app=None):
     self.app = app
-
+    
+    
+  # TODO: turn this into a before_request hook.
   def connect(self): 
     try:
       # The prefix component of the metric depends on the config/environment variable CLUSTER_NAME. 'heroku config:set'
       prefix = os.environ['CLUSTER_NAME']
     except:
-      prefix = 'no_cluster_name_defined'
+      prefix = None
     
     try:
       # This is the suffix component of the metric name. It gets the UUID which is also the hostname.
       suffix =  "heroku_" + os.popen('hostname').read().rstrip()
     except:
-      suffix = 'no_suffix_hostname_defined'
+      suffix = None
     
-    try:
-      # Where we send our stats
-      host = os.environ['STATSD_HOSTNAME']
-    except:
-      host = 'statsd-development-iad.lyft.net'
-        
-    port = 8127
-      
-    return statsd.StatsClient(host=host, port=port, prefix=prefix, suffix=suffix)
+
+    return statsd.StatsClient(host=os.environ.get('STATSD_HOSTNAME', None),
+                              port=os.environ.get('STATSD_PORT', None),
+                              prefix=os.environ.get('CLUSTER_NAME', None),
+                              suffix=suffix)
