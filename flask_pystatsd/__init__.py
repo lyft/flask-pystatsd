395af5cda0e3fc79b2c7815361ad4616074d9e95
elijahchancey <elijah.chancey@lyft.com>
2013-11-18 12:41:11 -0800

    .

--- flask_pystatsd/__init__.py
+++ flask_pystatsd/__init__.py
@@ -5,14 +5,18 @@ from flask import current_app
 Class StatsdApp(object):
     def __init__(self, app=None):
         self.app = app
+
+    def init_app(self, app):
         try:
             # This is the suffix component of the metric name. It gets the UUID which is also the hostname.
             suffix = "heroku_" + os.popen('hostname').read().rstrip()
         except:
             suffix = None
 
-    #return statsd.StatsClient(host=os.environ.get('STATSD_HOSTNAME', None),
-    current_app.statsd = statsd.StatsClient(host=os.environ.get('STATSD_HOSTNAME', None),
-    port=os.environ.get('STATSD_PORT', None),
-    prefix=os.environ.get('CLUSTER_NAME', None),
-    suffix=suffix)
+        app.before_request(self._before_request)
+
+    def _before_request(self):
+        current_app.statsd = statsd.StatsClient(host=os.environ.get('STATSD_HOSTNAME', None),
+        port=os.environ.get('STATSD_PORT', None),
+        prefix=os.environ.get('CLUSTER_NAME', None),
+        suffix=suffix)
